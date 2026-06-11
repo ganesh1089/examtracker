89533c16d50a367894e95d41fece1d60eb82f8ce
Ganesh Saini <trndganesh@gmail.com>
2026-06-11 20:51:29 +0530

    Fix signup route and PWA icons

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,6 +4,50 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
+
+router.post("/signup", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        message: "All fields required"
+      });
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists"
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword
+    });
+
+    res.status(201).json({
+      message: "Signup successful",
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email
+      }
+    });
+
+  } catch (err) {
+    console.log("SIGNUP ERROR:", err);
+
+    res.status(500).json({
+      message: "Server Error"
+    });
+  }
+});
 // ================= LOGIN =================
 router.post("/login", async (req, res) => {
 
